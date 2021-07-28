7d2ea1cab2440ec48e5c50ee1edf9a19d3f27add
Caio <caiotony16@gmail.com>
2021-07-28 15:47:47 +0100

    refactor: desacouple web and worker processes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Queue, Worker, Job } from 'bullmq';
+import { Queue } from 'bullmq';
 import IORedis from 'ioredis';
 import {REDIS_PORT, REDIS_HOST, REDIS_PASSWORD} from '../env';
 import express from 'express';
@@ -18,17 +18,7 @@ function worker() {
     const queue = new Queue('hi', {connection: redis_connection});
     queue.add('statistics', req.body)
   });
-  new Worker('hi', async (job: Job) => {
-    const commandsPipe = job.data;
-    await redis_connection.pipeline([
-      [commandsPipe[0].command, commandsPipe[0].arguments as string],
-      [commandsPipe[1].command, commandsPipe[1].arguments as string],
-      [commandsPipe[2].command, commandsPipe[2].arguments as string],
-      [commandsPipe[3].command, commandsPipe[3].arguments as string],
-      [commandsPipe[4].command, commandsPipe[4].arguments as string],
-      [commandsPipe[5].command, commandsPipe[5].arguments as string],
-    ]).exec()
-  }, {connection: redis_connection});
+  
 
   app.listen(process.env.PORT);
 }
---
+++ src/worker.ts
@@ -0,0 +1,30 @@
+import { Queue, Worker, Job } from 'bullmq';
+import IORedis from 'ioredis';
+import {REDIS_PORT, REDIS_HOST, REDIS_PASSWORD} from '../env';
+const redis_connection: IORedis.Redis = new IORedis(REDIS_HOST, {
+  password: REDIS_PASSWORD,
+  port: REDIS_PORT as unknown as number
+});
+import throng from 'throng';
+var WORKERS = process.env.WEB_CONCURRENCY || 4;
+
+async function worker () {
+  new Worker('hi', async (job: Job) => {
+    const commandsPipe = job.data;
+    await redis_connection.pipeline([
+      [commandsPipe[0].command, commandsPipe[0].arguments as string],
+      [commandsPipe[1].command, commandsPipe[1].arguments as string],
+      [commandsPipe[2].command, commandsPipe[2].arguments as string],
+      [commandsPipe[3].command, commandsPipe[3].arguments as string],
+      [commandsPipe[4].command, commandsPipe[4].arguments as string],
+      [commandsPipe[5].command, commandsPipe[5].arguments as string],
+    ]).exec()
+  }, {connection: redis_connection});
+}
+
+throng({
+  worker: worker,
+  count: WORKERS as number,
+  lifetime: Infinity
+});
+
